3f01ff6e0b137feba30a8690d91e8c4b1bd48a98
rajan savaliya <rajanpatel99094@gmail.com>
2020-06-06 10:58:29 +0530

    Updates

---
+++ precache-manifest.fe48a4877fbcc64c780073203c2407ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81aeb65dc181c68771fc0eef871e40c",
+    "url": "/react-pizza-motion-nn/index.html"
+  },
+  {
+    "revision": "2092d915a5328384bd07",
+    "url": "/react-pizza-motion-nn/static/css/main.fed5a396.chunk.css"
+  },
+  {
+    "revision": "0cedb0ce819cd5c6acdc",
+    "url": "/react-pizza-motion-nn/static/js/2.48bb8971.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-pizza-motion-nn/static/js/2.48bb8971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2092d915a5328384bd07",
+    "url": "/react-pizza-motion-nn/static/js/main.b0b6743e.chunk.js"
+  },
+  {
+    "revision": "cb04bf411c15f55e40c9",
+    "url": "/react-pizza-motion-nn/static/js/runtime-main.59146268.js"
+  }
+]);
